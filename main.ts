59a444652d3a1cc35cbaca5169e99706f74aa65d
alikashi11 <165997080+alikashi11@users.noreply.github.com>
2024-04-06 22:52:35 +0500

    push my project to github

---
+++ main.ts
@@ -0,0 +1,35 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+let todos = [];
+let condition = true;
+
+
+
+while(condition) 
+    {
+
+
+
+let addTask = await inquirer.prompt(
+
+    [
+        {
+            name:"todo",
+            type:"input",
+            message:"what do you want to add in your Todods",
+        },
+        {
+            name:"addMore",
+            type:"confirm",
+            message:"Do you want to add more ?",
+            default:"flase"
+        }
+    ]
+)
+todos.push(addTask.todo);
+condition = addTask.addMore
+console.log(todos)
+
+
+}
